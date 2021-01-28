04a5e8d2305b146fe2cd2fad038614e4b085296b
sdaLeventKilic <leventkilic@gmail.com>
2021-01-28 16:24:38 +0300

    Northwind SQL

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿use Northwind
+select * from Orders
+Select CategoryID,count(*) from products group by CategoryID
+Select CategoryID,count(*) from products group by CategoryID having count(*) <10
+Select CategoryID,count(*) from products where Unitprice>20 group by CategoryID having count(*) <10
+Select Ürünler.ProductID as Sıra , Ürünler.ProductName as "Ürün Adı" , Ürünler.Unitprice as Fiyatı ,Kategori.CategoryName as Kategori from Products as Ürünler inner join Categories as Kategori on Ürünler.CategoryID = Kategori.CategoryID 
+Select * from Products p left join [Order Details] od on p.ProductID=od.ProductID inner join Orders o on o.OrderID=od.OrderID
+/*SQL  Query on Northwind*/
+Select * from Customers c left join Orders o on c.CustomerID=o.CustomerID where o.CustomerID is null 
